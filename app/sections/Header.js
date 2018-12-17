8c12c3d06091c8a1094fc3a5dc4c8581ee7180b6
Shubhra Mittal <shubhra.goel@gmail.com>
2018-12-17 12:29:18 -0800

    added basic header

---
+++ app/sections/Header.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+    headText: {
+        textAlign: "right",
+        color: "#ffffff",
+        fontSize: 20,
+    },
+    headStyle: 
+    {
+        paddingTop: 50,
+        paddingBottom: 20,
+        paddingRight: 10,
+        backgroundColor: "#35605a",
+        flex:1
+    }
+})
+
+class Header extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         isLoggedIn: false
+      };
+    };
+    toggleUser = () =>  this.setState(previousState =>( {isLoggedIn : !previousState.isLoggedIn}))
+    
+    state = {  }
+    render() { 
+        let display = this.state.isLoggedIn? "Sample User" : this.props.message;
+        return (  
+            <View style = {styles.headStyle}>
+            <Text  style = {styles.headText}onPress = {this.toggleUser}>{display}</Text>
+            </View>
+        );
+    }
+}
+ 
+export default Header;
---
+++ app/views/Home.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text,View } from 'react-native';
+import Header from '../sections/Header'
+
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( 
+
+            <View style = {styles.container}>
+                <Header message = "Press to login" />
+                <Text style = {{flex:8}}>This will be homepgae</Text>
+                <Text style = {{flex:8}}>This will be homepgae</Text>
+                <Text style = {{flex:8}}>This will be homepgae</Text>
+                
+                
+            </View>
+         );
+    }
+}
+ 
+export default Home;
+
+const styles = StyleSheet.create({
+    container : {
+        flex:1
+    }
+})
